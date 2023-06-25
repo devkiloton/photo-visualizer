e4011b4eed55290a00b9030f5da1ac9e6288b5a1
devkiloton <dev.kiloton@gmail.com>
2023-06-25 15:57:22 -0300

    Creating sign up flow

--- src/app/services/sign-up.service.ts
+++ src/app/services/sign-up.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { NewUser } from '../types/newUser';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,9 @@ export class SignUpService {
   
   checkUserNameTaken(userName: string) {
     return this.http.get(`http://localhost:3000/user/exists/${userName}`)
+  }
+
+  signUp(newUser: NewUser) {
+    return this.http.post('http://localhost:3000/user/signup', newUser)
   }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormErrorMessageComponent } from '../form-error-message/form-error-message.component';
 import { NgIf } from '@angular/common';
 import { lowerCaseValidator } from '../validators/lowerCase.validator';
 import { UserNotTakenValidatorService } from '../validators/user-not-taken-validator.service';
+import { NewUser } from '../types/newUser';
+import { SignUpService } from '../services/sign-up.service';
 
 @Component({
   templateUrl: './sign-up.component.html',
@@ -16,6 +18,8 @@ export class SignUpComponent {
 
   fb = inject(NonNullableFormBuilder)
   userNotTakenValidatorService = inject(UserNotTakenValidatorService)
+  signUpService = inject(SignUpService)
+  router = inject(Router)
 
   signUpForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -23,5 +27,10 @@ export class SignUpComponent {
     userName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(255), lowerCaseValidator], this.userNotTakenValidatorService.checkUserNameTaken()],
     password: ['',[Validators.required, Validators.minLength(8), Validators.maxLength(14)]]
   })
+  
+  signup(){
+    const newUser = this.signUpForm.getRawValue() as NewUser;
+    return this.signUpService.signUp(newUser).subscribe(() => this.router.navigate(['']))
+  }
 
 }
